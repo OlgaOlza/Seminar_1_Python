8dd6d43c1c0ad5ced953988d43196b51048e0a3a
OlgaOlza <olga-rgb06@rambler.ru>
2023-09-14 12:34:53 +0300

    Лекция 2

--- main.py
+++ main.py
@@ -164,19 +164,170 @@
 #         line += "+"
 #         print(line)
 
-text = 'съешь ещё этих мягких французских булок'
-print(len(text))
-print(text[0]) # c
-print(text[1]) # ъ
-print(text[len(text)-1]) # к
-print(text[-5]) # б
-print(text[:]) # съешь ещё этих мягких французских булок
-print(text[:2]) # съ
-print(text[len(text)-2:]) # ок
-print(text[2:9]) # ешь ещё
-print(text[6:-18]) # ещё этих мягких
-print(text[0:len(text):6]) # сеикакл
-print(text[::6]) # сеикакл
-text = text[2:9] + text[-5] + text[:2] # ...
+# text = 'съешь ещё этих мягких французских булок'
+# print(len(text))
+# print(text[0]) # c
+# print(text[1]) # ъ
+# print(text[len(text)-1]) # к
+# print(text[-5]) # б
+# print(text[:]) # съешь ещё этих мягких французских булок
+# print(text[:2]) # съ
+# print(text[len(text)-2:]) # ок
+# print(text[2:9]) # ешь ещё
+# print(text[6:-18]) # ещё этих мягких
+# print(text[0:len(text):6]) # сеикакл
+# print(text[::6]) # сеикакл
+# text = text[2:9] + text[-5] + text[:2] # ...
 
+#  Лекция 2 
  
+#  Списки
+
+# # list_1 = []
+# # list_1 = list()
+# # # print(list_1)
+ 
+# list_1 = [1, 2, 3, 6] 
+# # print(*list_1)
+
+# # for i in list_1:
+# #     print(i)
+    
+# # print(len(list_1))
+
+# print(list_1[2])
+
+# print(list_1[-1])
+
+
+# list_1 = [1, 3, 5]
+# print(list_1)
+
+# list_1.append(8)
+# print(list_1)
+
+# list = []
+# for i in range(5):
+#     list.append(i)
+#     print(list)
+    
+    # Удаление последнего значения 
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.pop()) # 0
+# print(list_1) # [12, 7, -1, 21]
+# print(list_1.pop()) # 21
+# print(list_1) # [12, 7, -1]
+# print(list_1.pop()) # -1
+# print(list_1) # [12, 7]
+# print(list_1.pop()) # -1
+# print(list_1)
+
+# Удаление конкретного элемента 
+
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.pop(0)) # 12
+# print(list_1) # [7, -1, 21, 0]
+    
+    
+    # Добавление элемента на нужную позицию
+    
+# list1 = [12, 7, -1, 21, 0]
+# print(list1.insert(2, 11))
+# print(list1) # [12, 7, 11, -1, 21, 0]
+
+
+# # ● Отрицательное число в индексе — счёт с конца списка
+# list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list_1[0]) # 1
+# print(list_1[1]) # 2
+# print(list_1[len(list_1)-1]) # 10
+# print(list_1[-5]) # 6
+# print(list_1[:]) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list_1[:2]) # [1, 2]
+# print(list_1[len(list_1)-2:]) #[9, 10]
+# print(list_1[2:9]) # [3, 4, 5, 6, 7, 8, 9]
+# print(list_1[6:-18]) # []
+# print(list_1[0:len(list_1):6]) # [1, 7]
+# print(list_1[::6]) # [1, 7]
+    
+
+# t = () # создание пустого кортежа
+# print(type(t)) # class <'tuple'>
+# t = (1,)
+# print(type(t))
+
+# t = (1)
+# print(type(t))
+# t = (28, 9, 1990)
+# print(type(t))
+
+
+# colors = ['red', 'green', 'blue']
+# print(colors) # ['red', 'green', 'blue']
+# t = tuple(colors)
+# print(t) # ('red', 'green', 'blue')
+
+# t = tuple(['red', 'green', 'blue'])
+# print(t[0]) # red
+# print(t[2]) # blue
+# for e in t:
+#     print(e) # red green blue
+# t[0] = 'black' # TypeError: 'tuple' object does not support(нельзя изменять
+# # кортеж)
+
+# d = {}
+# в = dict()
+# print(d)
+
+# d['q'] = 'qwerrty'
+# print(d)
+
+# d['w'] = 'werrty'
+# print(d['q'])
+
+# dictionary = {}
+# dictionary ={'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+# print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+# print(dictionary['left']) # ←
+
+# # типы ключей могут отличаться
+# print(dictionary['up']) # ↑
+# # типы ключей могут отличаться
+
+# dictionary['left'] = '⇐'
+# print(dictionary['left']) # ⇐
+# print(dictionary['type']) # KeyError: 'type'
+
+# # del dictionary['left'] # удаление элемента
+
+# # for item in dictionary: # for (k,v) in dictionary.items():
+# #     print('{}: {}'.format(item, dictionary[item]))
+# # # up: ↑
+# # # down: ↓
+# # # right: →
+    
+# colors = {'red', 'green', 'blue'}
+# print(colors) # {'red', 'green', 'blue'}
+# colors.add('red')
+# print(colors) # {'red', 'green', 'blue'}
+# colors.add('gray')
+# print(colors) # {'red', 'green', 'blue','gray'}
+# colors.remove('red')
+# print(colors) # {'green', 'blue','gray'}
+# colors.remove('red') # KeyError: 'red'
+# colors.discard('red') # ok
+# print(colors) # {'green', 'blue','gray'}
+# colors.clear() # { }
+# print(colors) # set()
+    
+    
+#     a = {1, 2, 3, 5, 8}
+# b = {2, 5, 8, 13, 21}
+# c = a.copy() # c = {1, 2, 3, 5, 8}
+# u = a.union(b) # u = {1, 2, 3, 5, 8, 13,
+# i = a.intersection(b) # i = {8, 2, 5}
+# dl = a.difference(b) # dl = {1, 3}
+# dr = b.difference(a) # dr = {13, 21}
+# q=a.union(b).difference(a.intersection(b)) # {1, 21, 3, 13}
+
+
